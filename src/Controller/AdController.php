0f011143060cc307dd2b5ac6110308d748b0e2ff
shamsia1 <alimishamsya@gmail.com>
2020-02-19 11:10:33 +0100

    initial commit

---
+++ src/Controller/AdController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)//injection de dependence
+    {
+
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+
+    /**
+     * permet d'afficher une seule announce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * paramconverteur(on change un parametere en une entity)
+     */
+    public function show(Ad $ad)
+    {
+        //je récupere l'announce qui correspond au slug !
+        //$ad = $repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+}
