662a0252cfb0352d83f3a4c941ec09b81ab227ce
shamsia1 <alimishamsya@gmail.com>
2020-03-03 10:29:51 +0100

    formulaire d'edition d'annouce

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -70,6 +70,49 @@ class AdController extends AbstractController
     }
 
     /**
+    * permet d'afficher le formulaire d'edition
+    *
+    * @Route("/ads/{slug}/edit", name="ads_edit")
+    */
+    public function edit(Ad $ad, Request $request)
+    {
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            foreach($ad->getImages() as $image){
+                $image->setAd($ad);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($image);
+            }
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash('success',
+
+             "Les modification de l'annouce <strong>{$ad->getTitle()}</strong> ont 
+
+              bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+           
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
+    }
+
+    /**
      * permet d'afficher une seule announce
      *
      * @Route("/ads/{slug}", name="ads_show")
@@ -82,6 +125,6 @@ class AdController extends AbstractController
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
-    }
+    } 
     
 }
