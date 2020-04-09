4cafe4eae552120efbbf92bd772d29db1c98e0af
shamsia1 <alimishamsya@gmail.com>
2020-04-09 15:43:59 +0200

    password encoder, randame avatars

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,24 +4,74 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Image;
-
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
+
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker= Factory::create('Fr-fr');
+
+        // Nous gérons les utilisateur
+
+        $users = [];
+
+        $genres = ['male', 'female'];
+
+        for ($i=1; $i <=10; $i++) { 
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg';
+            
+
+            // condition terner
+            $picture .= ($genre == 'male' ? 'men/' : 'women/') .$pictureId; 
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+            
+            $user->setFirstName($faker->firstname($genre))
+                ->setLastName($faker->lastname)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'. join('</p><p>', $faker->paragraphs(3)). '</p>')
+                ->setHash($hash)
+                ->setPicture($picture);
+
+
+            $manager->persist($user);
+            $users[]= $user;
+
+        }
+        // Nous gérons les announces
         //$slugify = new Slugify();
-        for($i = 1; $i <=30; $i++){
+        for($i=1; $i<=30; $i++){
         $ad = new Ad;
         $title = $faker->sentence();
         //$slug = $slugify->slugify($title);
         $coverImage = $faker->imageUrl(1000,350);
         $introduction = $faker->paragraph(2);
-        $content= '<p>'. join('<p></p>', $faker->paragraphs(5)). '</p>';
+        $content= '<p>'. join('</p><p>', $faker->paragraphs(5)). '</p>';
+
+        $user = $users[mt_rand(0, count($users) - 1)];
 
         $ad->setTitle($title)
            // ->setSlug($slug)
@@ -30,7 +80,8 @@ class AppFixtures extends Fixture
             ->setContent($content)
             //rand génére une valeur aléatoire
             ->setPrice(mt_rand(40, 200))
-            ->setRooms(mt_rand(1,5));
+            ->setRooms(mt_rand(1,5))
+            ->setAuthor($user);
 
         for($j = 1; $j <= mt_rand(2,5); $j++)
         {
