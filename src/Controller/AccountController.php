1a7fe4ad53c4e0997906be2b293d5209872e72f8
shamsia1 <alimishamsya@gmail.com>
2020-04-24 15:15:11 +0200

    securiser le accountcontroller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\PasswordUpdate;
 use App\Entity\User;
 use App\Form\AccountType;
-use App\Form\PasswordUpdateType;
+use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -85,6 +86,7 @@ class AccountController extends AbstractController
      * permet d'afficher et de traiter le formulaire de modification de profile
      * 
      * @Route("/account/profile", name="account_profile")
+     * @IsGranted('ROLE_USER')
      * 
      * @return Response
      */
@@ -113,6 +115,7 @@ class AccountController extends AbstractController
      * permet de modifier le mot de pass
      * 
      * @Route("/account/password-update", name="account_password")
+     * @IsGranted('ROLE_USER')
      * @return Response
      */
     public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder){
@@ -161,6 +164,7 @@ class AccountController extends AbstractController
      * permet d'afficher le profil de l'utilisateur connecté
      * 
      * @Route("/account", name="account_index")
+     * @IsGranted('ROLE_USER')
      * 
      * @return Response
      */
