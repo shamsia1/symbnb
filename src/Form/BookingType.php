730444ead9352bee1d82809dc555b5178b6b799d
shamsia1 <alimishamsya@gmail.com>
2020-05-11 15:12:04 +0200

    Utiliser un calendrier Javascript pour empêcher le choix de dates indisponibles via bootstrap datepicker uxsolution

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,21 +4,22 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use App\Form\ApplicationType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate', DateType::class, $this->getConfiguration(
-                "Date d'arriver", "La date a laquelle vous comptez d'arriver", ["widget" =>"single_text"]
+            ->add('startDate', TextType::class, $this->getConfiguration(
+                "Date d'arriver", "La date a laquelle vous comptez d'arriver"
             ))
 
-            ->add('endDate', DateType::class, $this->getConfiguration("Date de depart", "La date a laquelle vous quittez les lieux", ["widget" =>"single_text"]))
+            ->add('endDate', TextType::class, $this->getConfiguration("Date de depart", "La date a laquelle vous quittez les lieux" ))
             
             ->add('comment', TextareaType::class, $this->getConfiguration(
             false, "si vous avez un commentaire, n'hésitez pas", ["required" => false]))
