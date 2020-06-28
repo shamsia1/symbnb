e09fe938a24aa88d442b55d2d8d3cebb517f832c
shamsia1 <alimishamsya@gmail.com>
2020-06-28 16:22:30 +0200

    Création du formulaire d'édition des annonces pour l'administration

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminAdController extends AbstractController
 {
@@ -16,5 +19,39 @@ class AdminAdController extends AbstractController
         return $this->render('admin/ad/index.html.twig', [
             'ads' =>$repo->findAll()
         ]);
+    }
+
+
+    /**
+     * 
+     * permet d'afficher le formulaire d'edition
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     * 
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request){
+        $form= $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted()&& $form->isValid() ){
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'announce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig',[
+            'ad' =>$ad,
+            'form'=>$form->createView()
+        ]);
+
     }
 }
