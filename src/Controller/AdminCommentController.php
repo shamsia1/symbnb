d5e4f8ba3f5e51a0e5af5cac7e119afe2cf0d4e8
shamsia1 <alimishamsya@gmail.com>
2020-07-23 16:01:21 +0200

    Solution  créez le formulaire d'édition d'un commentaire

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\AdminCommentType;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,6 +22,40 @@ class AdminCommentController extends AbstractController
         return $this->render('admin/comment/index.html.twig', [
             'comments' => $comments,
         ]);
+    }
+
+
+    /**
+     * permet de modifier un commentaire
+     * 
+     * @Route("/admin/comments/{id}/edit", name="admin_comment_edit")
+     * 
+     * @return Response 
+     */
+    public function edit(Comment $comment, Request $request){
+
+        $form = $this->createForm(AdminCommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire numéro {$comment->getId()} a bien été modifié"
+
+                );
+        }
+
+        return $this->render('admin/comment/edit.html.twig',[
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]); 
+
     }
 }
  
